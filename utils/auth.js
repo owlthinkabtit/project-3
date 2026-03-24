74b5642c47d8ee5bf4c2f60209a2e529f59db248
Meek Meek <haggins.jameka@gmail.com>
2026-03-24 15:30:04 -0400

    had to verify the registration flow and implement autho middlware

--- utils/auth.js
+++ utils/auth.js
@@ -1,2 +1,25 @@
 import jwt from 'jsonwebtoken';
 
+export async function authMiddleware(req, res, next) {
+  try {
+    let token = req.headers.authorization
+
+    if (!token) {
+      return res.status(403).json({ message: "Access denined. No token provided" })
+    }
+
+    token = token.split(' ').pop().trim()
+
+    const { data } = jwt.verify(token, secret)
+
+    req.user = data
+
+    next();
+
+  } catch (err) {
+    console.log(err.message)
+    res.status(400).json({ message: err.message })
+  }
+}
+
+export default authMiddleware;
