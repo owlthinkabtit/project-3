79f8406fabe0facd17d205ff2fab534f22428722
Meek Meek <haggins.jameka@gmail.com>
2026-03-24 15:40:23 -0400

    Added the Project model also made sure that the project is linked to the user that created it. Next going to create the route for the projects.

---
+++ models/Project.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String, 
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+ 
+}, { timestamps: true});
+
+const Project = mongoose.model('Project', projectSchema);
+
+export default Project
